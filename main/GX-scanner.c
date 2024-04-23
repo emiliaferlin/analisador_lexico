8b56e78ab0e0864eaa8f5016797c8275c8997491
Emilia <emilia.ferlin@sbsistemas.com.br>
2024-04-22 21:08:20 -0300

    implementa automato a.1

--- main/GX-scanner.c
+++ main/GX-scanner.c
@@ -15,7 +15,53 @@ int main() {
 
     // Lê e exibe cada linha do arquivo
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        printf("%s", linha);
+        // Remove o caractere de nova linha, se existir
+
+        int cont_linha = 0; // Comprimento da linha
+         for (int i = 0; linha[i] != '\0'; i++) {
+            cont_linha++;
+        }
+
+        int state = 0;
+        int i = 0;
+
+        while (i < cont_linha) {
+            printf("\n", linha[i]);
+            switch (state) {
+                case 0:
+                    if (linha[i] == '{') {
+                        printf("%c\n", linha[i]);
+                        printf("Acerto no estado 0\n");
+                        state = 1;
+                    } else {
+                        printf("%c\n", linha[i]);
+                        // error
+                        printf("Erro no esatdo 0\n");
+                        state = 0;
+                    }
+                    break;
+
+                case 1:
+                    if (linha[i] == '}') {
+                        printf("%c\n", linha[i]);
+                        printf("Acerto no estado 1\n");
+                        state = 2;
+                    } else {
+                        printf("%c\n", linha[i]);
+                        // error
+                        printf("Erro no estado 1\n");
+                        state = 1;
+                    }
+                    break;
+            }
+            i++;
+        }
+
+        if (state == 2) {
+            printf("Acepet: Cadeia aceita\n");
+        } else {
+            printf("Error: Cadeia não reconhecida\n");
+        }
     }
 
     // Fecha o arquivo após a leitura
