4f406dbd2cca7c6f5076857bc7e63de14668652a
Emilia <emilia.ferlin@sbsistemas.com.br>
2024-04-24 21:47:47 -0300

    finalizando trabalho

--- main/GX-scanner.c
+++ main/GX-scanner.c
@@ -2,7 +2,7 @@
 
 int main() {
     FILE *arquivo;
-    char linha[100]; // Tamanho máximo de cada linha
+    char linha[300]; // Tamanho máximo de cada linha
 
     // Abre o arquivo em modo de leitura ("r")
     arquivo = fopen("GX-cadeias_entrada.txt", "r");
@@ -17,43 +17,36 @@ int main() {
 
     // Lê cada linha do arquivo
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        //variavel para mostrar o #linha
         numLinha++;
 
-        // Remove o caractere de nova linha, se existir
-        if (linha[strlen(linha) - 1] == '\n')
-            linha[strlen(linha) - 1] = '\0';
 
          int cont_linha = 0; // Comprimento da linha
          for (int i = 0; linha[i] != '\0'; i++) {
             cont_linha++;
         }
 
+        //remove o pula linha, assim dando o valor real de caracteres
+        cont_linha = cont_linha - 1;
+
+        //a.1
         int state = 0;
         int i = 0;
-
         printf("\n");
         while (i < cont_linha) {
             switch (state) {
                 case 0:
                     if (linha[i] == '{') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
                         state = 1;
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no esatdo 0\n");
                         state = 3;
                     }
                     break;
 
                 case 1:
                     if (linha[i] == '}') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
                         state = 2;
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         state = 1;
                     }
                     break;
@@ -76,14 +69,13 @@ int main() {
             printf("\n");
         }
 
+        //b.1
         int stateb1 = 0;
         i = 0;
          while (i < cont_linha) {
             switch (stateb1) {
                 case 0:
                     if ((linha[i] >= 'A' && linha[i] <= 'Z') || (linha[i] >= 'a' && linha[i] <= 'z')) {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
                         if((linha[i + 1] >= 'A' && linha[i + 1] <= 'Z') || (linha[i + 1] >= 'a' && linha[i + 1] <= 'z')){
                             stateb1 = 0;
                         }else{
@@ -91,8 +83,6 @@ int main() {
                         }
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
                         stateb1 = 1;
                     }
 
@@ -100,8 +90,6 @@ int main() {
 
                 case 1:
                     if ((linha[i] >= '0' && linha[i] <= '9') || linha[i] == '_') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
                         if(cont_linha == (i+1)){
                             stateb1 = 2;
                         }else{
@@ -113,8 +101,6 @@ int main() {
                         }
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         stateb1 = 0;
                     }
 
@@ -124,8 +110,7 @@ int main() {
             i++;
         }
 
-
-        if (state == 2) {
+        if (stateb1 == 2) {
             printf("#%d", numLinha);
             printf(" --> TKN_VAR --> ");
             printf("Accept --> ");
@@ -139,19 +124,16 @@ int main() {
             printf("\n");
         }
 
+        //b.2
         int stateb2 = 0;
         i = 0;
          while (i < cont_linha) {
             switch (stateb2) {
                 case 0:
                     if (linha[i] == 'i') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
                         stateb2 = 1;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
                         stateb2 = 3;
                     }
 
@@ -159,13 +141,9 @@ int main() {
 
                 case 1:
                     if (linha[i] == 'f') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
                         stateb2 = 2;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         stateb2 = 3;
                     }
 
@@ -177,7 +155,7 @@ int main() {
         }
 
 
-         if (state == 2) {
+         if (stateb2 == 2) {
             printf("#%d", numLinha);
             printf(" --> TKN_RESWORD --> ");
             printf("Accept --> ");
@@ -191,23 +169,24 @@ int main() {
             printf("\n");
         }
 
+        //c.2
         int statec2 = 0;
         i = 0;
          while (i < cont_linha) {
             switch (statec2) {
                 case 0:
                     if (linha[i] == '<' || linha[i] == '>' || linha[i] == '!' || linha[i] == '=') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
-                        if(linha[i] == '=' && cont_linha == 1){
-                            statec2 = 2;
+                        if(linha[i] == '='){
+                            if(cont_linha == 1){
+                                statec2 = 2;
+                            }else{
+                                statec2 = 3;
+                            }
                         }else{
                             statec2 = 1;
                         }
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
                         statec2 = 3;
                     }
 
@@ -215,13 +194,13 @@ int main() {
 
                 case 1:
                     if (linha[i] == '=') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
-                        statec2 = 2;
+                        if(cont_linha == (i+1)){
+                            statec2 = 2;
+                        }else{
+                            statec2 = 3;
+                        }
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         statec2 = 3;
                     }
 
@@ -231,8 +210,8 @@ int main() {
             i++;
         }
 
-        //c.2) Comparações: TKN_LTEQ (<=), TKN_GTEQ (>=), TKN_EQ (=), TKN_DIFF (!=) REVER
-         if (state == 2) {
+        //KN_LTEQ (<=), TKN_GTEQ (>=), TKN_EQ (=), TKN_DIFF (!=) REVER
+         if (statec2 == 2) {
             printf("#%d", numLinha);
             printf(" --> TKN_LTEQ --> ");
             printf("Accept --> ");
@@ -246,19 +225,17 @@ int main() {
             printf("\n");
         }
 
+
+        //c.4
         int statec4 = 0;
         i = 0;
          while (i < cont_linha) {
             switch (statec4) {
                 case 0:
                     if (linha[i] == '=') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
                         statec4 = 1;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
                         statec4 = 3;
                     }
 
@@ -266,13 +243,9 @@ int main() {
 
                 case 1:
                     if (linha[i] == ':') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
                         statec4 = 2;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         statec4 = 3;
                     }
 
@@ -282,8 +255,8 @@ int main() {
             i++;
         }
 
-        //c4
-         if (state == 2) {
+
+         if (statec4 == 2) {
             printf("#%d", numLinha);
             printf(" --> TKN_ASSIGN --> ");
             printf("Accept --> ");
@@ -297,20 +270,43 @@ int main() {
             printf("\n");
         }
 
-        int statee1 = 0;
+        //d.3
+        int stated3 = 0;
         i = 0;
          while (i < cont_linha) {
-            switch (statee1) {
+            switch (stated3) {
                 case 0:
-                    if (linha[i] == ';') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
-                        statee1 = 1;
+                    if (linha[i] == '"') {
+                        stated3 = 1;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
-                        statee1 = 2;
+                        stated3 = 4;
+                    }
+
+                    break;
+
+                case 1:
+                    if (linha[i] == ' '|| linha[i] == '!' || linha[i] == '#' || linha[i] == '$' || linha[i] == '%' || linha[i] == '&' ||
+                        linha[i] == "'" || linha[i] == '(' || linha[i] == ')' || linha[i] == '*' || linha[i] == '+' || linha[i] == ',' ||
+                        linha[i] == '-' || linha[i] == '.' || linha[i] == '/' || linha[i] == ':' || linha[i] == ';' || linha[i] == '<' ||
+                        linha[i] == '=' || linha[i] == '>' || linha[i] == '?' || linha[i] == '@' || linha[i] == '[' || linha[i] == ']' ||
+                        linha[i] == '^' || linha[i] == '_' || linha[i] == '`' || linha[i] == '{' || linha[i] == '|' || linha[i] == '}' ||
+                        linha[i] == '~' || (linha[i] >= 'A' && linha[i] <= 'Z') || (linha[i] >= 'a' && linha[i] <= 'z') ||
+                        (linha[i] >= '0' && linha[i] <= '9')) {
+                        stated3 = 1;
+
+                    } else {
+                        stated3 = 2;
+                    }
+
+                    break;
+
+                case 2:
+                    if (linha[i] == '"') {
+                        stated3 = 3;
+
+                    } else {
+                        stated3 = 4;
                     }
 
                     break;
@@ -319,8 +315,100 @@ int main() {
             i++;
         }
 
-        //e1
-        if (state == 2) {
+         if (stated3 == 3) {
+            printf("#%d", numLinha);
+            printf(" --> TKN_NUMREAL --> ");
+            printf("Accept --> ");
+            printf("%s ", linha);
+            printf("\n");
+        } else {
+            printf("#%d", numLinha);
+            printf(" --> TKN_NUMREAL --> ");
+            printf("Reject --> ");
+            printf("%s ", linha);
+            printf("\n");
+        }
+
+
+        //d.6
+        int stated6 = 0;
+        i = 0;
+         while (i < cont_linha) {
+            switch (stated6) {
+                case 0:
+                    if (linha[i] == '"') {
+                        stated6 = 1;
+
+                    } else {
+                        stated6 = 4;
+                    }
+
+                    break;
+
+                case 1:
+                    if (linha[i] == ' '|| linha[i] == '!' || linha[i] == '#' || linha[i] == '$' || linha[i] == '%' || linha[i] == '&' ||
+                        linha[i] == "'" || linha[i] == '(' || linha[i] == ')' || linha[i] == '*' || linha[i] == '+' || linha[i] == ',' ||
+                        linha[i] == '-' || linha[i] == '.' || linha[i] == '/' || linha[i] == ':' || linha[i] == ';' || linha[i] == '<' ||
+                        linha[i] == '=' || linha[i] == '>' || linha[i] == '?' || linha[i] == '@' || linha[i] == '[' || linha[i] == ']' ||
+                        linha[i] == '^' || linha[i] == '_' || linha[i] == '`' || linha[i] == '{' || linha[i] == '|' || linha[i] == '}' ||
+                        linha[i] == '~' || (linha[i] >= 'A' && linha[i] <= 'Z') || (linha[i] >= 'a' && linha[i] <= 'z') ||
+                        (linha[i] >= '0' && linha[i] <= '9')) {
+                        stated6 = 1;
+
+                    } else {
+                        stated6 = 2;
+                    }
+
+                    break;
+
+                case 2:
+                    if (linha[i] == '"') {
+                        stated6 = 3;
+
+                    } else {
+                        stated6 = 4;
+                    }
+
+                    break;
+
+            }
+            i++;
+        }
+
+         if (stated6 == 3) {
+            printf("#%d", numLinha);
+            printf(" --> TKN_STR --> ");
+            printf("Accept --> ");
+            printf("%s ", linha);
+            printf("\n");
+        } else {
+            printf("#%d", numLinha);
+            printf(" --> TKN_STR --> ");
+            printf("Reject --> ");
+            printf("%s ", linha);
+            printf("\n");
+        }
+
+        //e.1
+        int statee1 = 0;
+        i = 0;
+         while (i < cont_linha) {
+            switch (statee1) {
+                case 0:
+                    if (linha[i] == ';') {
+                        statee1 = 2;
+
+                    } else {
+                        statee1 = 1;
+                    }
+
+                    break;
+
+            }
+            i++;
+        }
+
+        if (statee1 == 2) {
             printf("#%d", numLinha);
             printf(" --> TKN_STMSEP --> ");
             printf("Accept --> ");
@@ -334,19 +422,16 @@ int main() {
             printf("\n");
         }
 
+        //e.2
         int statee2 = 0;
         i = 0;
          while (i < cont_linha) {
             switch (statee2) {
                 case 0:
                     if (linha[i] == '(') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 0\n");
                         statee2 = 1;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 0\n");
                         statee2 = 3;
                     }
 
@@ -354,13 +439,9 @@ int main() {
 
                 case 1:
                     if (linha[i] == ')') {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Acerto no estado 1\n");
                         statee2 = 2;
 
                     } else {
-                        printf("Caractere ==> %c ", linha[i]);
-                        printf("Erro no estado 1\n");
                         statee2 = 1;
                     }
 
@@ -370,20 +451,53 @@ int main() {
             i++;
         }
 
-        //e.2) Parênteses: TKN_LEFTPAR, TKN_RIGHTPAR ( (...) )
-         if (state == 2) {
+         if (statee2 == 2) {
             printf("#%d", numLinha);
-            printf(" --> TKN_LEFTPAR --> ");
+            printf(" --> TKN_LEFTPAR, TKN_RIGHTPAR --> ");
             printf("Accept --> ");
             printf("%s ", linha);
             printf("\n");
         } else {
             printf("#%d", numLinha);
-            printf(" --> TKN_LEFTPAR --> ");
+            printf(" --> TKN_LEFTPAR, TKN_RIGHTPAR --> ");
             printf("Reject --> ");
             printf("%s ", linha);
             printf("\n");
         }
+
+        //e.7
+        int statee7 = 0;
+        i = 0;
+         while (i < cont_linha) {
+            switch (statee7) {
+                case 0:
+                    if (linha[i] == ' ' || linha[i] == '\t' || linha[i] == '\n' || linha[i] == '\r') {
+                        statee7 = 1;
+
+                    } else {
+                        statee7 = 3;
+                    }
+
+                    break;
+
+            }
+            i++;
+        }
+
+         if (statee7 == 1) {
+            printf("#%d", numLinha);
+            printf(" --> TKN_REJECT --> ");
+            printf("Accept --> ");
+            printf("%s ", linha);
+            printf("\n");
+        } else {
+            printf("#%d", numLinha);
+            printf(" --> TKN_REJECT --> ");
+            printf("Reject --> ");
+            printf("%s ", linha);
+            printf("\n");
+        }
+
 
 
     }
@@ -393,3 +507,5 @@ int main() {
 
     return 0;
 }
+
+
